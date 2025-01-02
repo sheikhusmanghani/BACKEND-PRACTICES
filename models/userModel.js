cfa94be06e6c0560463676b1f219ed06a78f11f1
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2025-01-02 18:46:34 +0500

    changed

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/AuthTestApp")
+  .connect("mongodb://localhost:27017/AuthTestApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB ok hy");
   })
