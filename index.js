98a9480b90c092c0ed101960c8baba757f1fb95c
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2025-01-03 17:09:10 +0500

    ok

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const UserModel = require("./models/userModel");
 const jwt = require("jsonwebtoken");
 const { ProtectedRoute, isUserExist } = require("./middlewares");
 const postModel = require("./models/postModel");
+const path = require("path");
 const PORT = 8000;
 
 const app = express();
 
 // // Middlewares
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
